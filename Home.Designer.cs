beddbc9419c80e74220f001b4d5d56de7e5fcf18
jwigl631 <jwigl631@evit.com>
2022-09-22 13:34:43 -0700

    Add project files.

---
+++ Home.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace pokedex
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.openButton = new CustomControls.RJControls.RJButton();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.openButton.BackgroundColor = System.Drawing.Color.DeepSkyBlue;
+            this.openButton.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.openButton.BorderRadius = 45;
+            this.openButton.BorderSize = 0;
+            this.openButton.FlatAppearance.BorderSize = 0;
+            this.openButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openButton.ForeColor = System.Drawing.Color.White;
+            this.openButton.Location = new System.Drawing.Point(5, 5);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(90, 90);
+            this.openButton.TabIndex = 0;
+            this.openButton.TextColor = System.Drawing.Color.White;
+            this.openButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomControls.RJControls.RJButton openButton;
+    }
+}
+
