324773b1fc1af5aa423de84f279419114ffbacad
jwigl631 <jwigl631@evit.com>
2022-09-22 14:04:46 -0700

    caught screen

--- caughtScreen.Designer.cs
+++ caughtScreen.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.caughtPokemonPic = new CustomControls.RJControls.RJCircularPictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.caughtPokemonPic = new CustomControls.RJControls.RJCircularPictureBox();
+            this.rjTextBox1 = new CustomControls.RJControls.RJTextBox();
+            this.caughtNickname = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.caughtPokemonPic)).BeginInit();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.caughtNickname);
+            this.flowLayoutPanel1.Controls.Add(this.rjTextBox1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 187);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(336, 251);
+            this.flowLayoutPanel1.TabIndex = 1;
             // 
             // caughtPokemonPic
             // 
@@ -48,12 +60,35 @@
             this.caughtPokemonPic.TabIndex = 0;
             this.caughtPokemonPic.TabStop = false;
             // 
-            // flowLayoutPanel1
+            // rjTextBox1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 187);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(336, 251);
-            this.flowLayoutPanel1.TabIndex = 1;
+            this.rjTextBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.rjTextBox1.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.rjTextBox1.BorderFocusColor = System.Drawing.Color.DeepSkyBlue;
+            this.rjTextBox1.BorderSize = 2;
+            this.rjTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.rjTextBox1.Location = new System.Drawing.Point(4, 21);
+            this.rjTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.rjTextBox1.Multiline = false;
+            this.rjTextBox1.Name = "rjTextBox1";
+            this.rjTextBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.rjTextBox1.PasswordChar = false;
+            this.rjTextBox1.Size = new System.Drawing.Size(250, 32);
+            this.rjTextBox1.TabIndex = 0;
+            this.rjTextBox1.Texts = "";
+            this.rjTextBox1.UnderlinedStyle = false;
+            // 
+            // caughtNickname
+            // 
+            this.caughtNickname.AutoSize = true;
+            this.caughtNickname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caughtNickname.ForeColor = System.Drawing.Color.White;
+            this.caughtNickname.Location = new System.Drawing.Point(3, 0);
+            this.caughtNickname.Name = "caughtNickname";
+            this.caughtNickname.Size = new System.Drawing.Size(164, 17);
+            this.caughtNickname.TabIndex = 1;
+            this.caughtNickname.Text = "Please enter a nickname";
             // 
             // caughtScreen
             // 
@@ -64,6 +99,8 @@
             this.Controls.Add(this.caughtPokemonPic);
             this.Name = "caughtScreen";
             this.Size = new System.Drawing.Size(336, 438);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.caughtPokemonPic)).EndInit();
             this.ResumeLayout(false);
 
@@ -73,5 +110,7 @@
 
         private CustomControls.RJControls.RJCircularPictureBox caughtPokemonPic;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label caughtNickname;
+        private CustomControls.RJControls.RJTextBox rjTextBox1;
     }
 }
