3f237ba0ea9e13d069a33336ea5f41796669e4c2
Ernie Clark <eclar971@evit.com>
2022-09-26 21:49:03 -0700

    fixed scroller issues

--- scollerSceen.cs
+++ scollerSceen.cs
@@ -23,6 +23,7 @@ namespace pokedex
 
         private void scollerSceen_Load(object sender, EventArgs e)
         {
+            pokemon = SqliteDataAccess.LoadPokemon();
             pokemonInfo.Text = pokemon[i].ToString();
             imagePath = $"..\\..\\pokeImg\\{pokemon[i].Num.ToString().PadLeft(3, '0')} (Custom).png";
             pokemonImage.Image = Image.FromFile(imagePath);
